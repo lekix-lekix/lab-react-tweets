5d929425eefd8384a34f6b331549f4cab02b70f0
lekix-lekix <pouliquen.k@gmail.com>
2023-02-27 17:09:59 +0100

    done

---
+++ src/components/Timestamp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Timestamp = (props) => {
+    const { time } = props;
+  return (
+    <span className="timestamp">{time}</span>
+  )
+}
+
+export default Timestamp
